7b6e172107d9e65befe501d1cbc03bb64614ddb7
papakonnekt <Rkoontz95@gmail.com>
2026-06-22 10:51:48 -0400

    Enable cleartext in Capacitor config to allow HTTP API requests

--- client/capacitor.config.ts
+++ client/capacitor.config.ts
@@ -5,7 +5,8 @@ const config: CapacitorConfig = {
   appName: 'MycoScheduler',
   webDir: 'dist',
   server: {
-    androidScheme: 'https'
+    androidScheme: 'https',
+    cleartext: true
   }
 };
 
